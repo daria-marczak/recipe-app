be3520e202d0588e1588f7e2dbd99c81968b86d7
daria marczak <marczak.ria@gmail.com>
2018-09-22 18:13:02 +0200

    Showing list of ingredients

--- src/containers/SingleRecipe.js
+++ src/containers/SingleRecipe.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Row, Col, Tag } from "antd";
+import { Row, Col, Tag, List } from "antd";
 import "../styles/RecipeList.css";
 
-
 class SingleRecipe extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: []
+    }
+  }
+
   componentDidMount() {
     const { recipes } = this.props;
 
-    if (!recipes) return <p>Loading</p>
+    if (!recipes) return <p>Loading</p>;
+
+    this.setState({
+      recipes
+    })
   }
 
   render() {
@@ -21,26 +31,35 @@ class SingleRecipe extends Component {
 
     const recipe = currentRecipe.recipe;
 
+    const ingredients = recipe.ingredientLines;
+
     return (
       <Row gutter={8}>
         <Col span={12}>
-          <img src={recipe.image}></img>
+          <img src={recipe.image} />
         </Col>
         <Col span={12}>
           <h2>{recipe.label}</h2>
           {recipe.healthLabels.map(label => {
-            return (<Tag color="cyan">{label}</Tag>)
+            return <Tag color="cyan">{label}</Tag>;
           })}
+          <List 
+            header={<h3>Ingredients</h3>}
+            bordered
+            size="small"
+            dataSource={ingredients}
+            renderItem={(item, index) => (<List.Item>{item}</List.Item>)}
+          />
         </Col>
       </Row>
-    )
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
     recipes: state.recipes.items
-  }
-}
+  };
+};
 
 export default connect(mapStateToProps)(SingleRecipe);
