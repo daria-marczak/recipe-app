661c20d9a2f812b203ad8ba59e22d29cc97a4b2c
daria marczak <marczak.ria@gmail.com>
2018-09-22 17:50:33 +0200

    Working on single recipe page

--- src/containers/SingleRecipe.js
+++ src/containers/SingleRecipe.js
@@ -1,20 +1,55 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Row, Col, Tag } from "antd";
+import "../styles/RecipeList.css";
+
 
 class SingleRecipe extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipe: []
+    }
+  }
+
+  componentDidMount() {
+
+    if (!recipes) return <p>Loading</p>
 
     const { recipes } = this.props;
-
-    if (!recipes) return;
-
     const [currentRecipe] = recipes.filter(recipe => {
       const id = recipe.recipe.totalWeight.toString();
       if (this.props.match.params.id === id) return recipe;
     });
-    console.log(currentRecipe);
 
-    return <p>I'm a single recipe</p>
+    const recipe = currentRecipe.recipe;
+
+    this.setState({
+      recipe: currentRecipe.recipe
+    });
+
+
+  }
+
+  renderIngredients() {
+    // console.log(recipes);
+  }
+
+  render() {
+
+    console.log(this.state);
+
+    return (
+      <Row gutter={8}>
+        <Col span={12}>
+          {/* <img src={recipe.image}></img> */}
+        </Col>
+        <Col span={12}>
+          {/* <h2>{recipe.label}</h2> */}
+          <Tag color="cyan"></Tag>
+        </Col>
+      </Row>
+    )
   }
 }
 
