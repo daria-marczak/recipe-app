85b5771444c40e17ccc24d20a962221d96813b56
daria marczak <marczak.ria@gmail.com>
2018-09-22 17:09:21 +0200

    Filtered recipes to show only one

--- src/containers/SingleRecipe.js
+++ src/containers/SingleRecipe.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class SingleRecipe extends Component {
   render() {
+
+    const { recipes } = this.props;
+
+    if (!recipes) return;
+
+    const [currentRecipe] = recipes.filter(recipe => {
+      const id = recipe.recipe.totalWeight.toString();
+      if (this.props.match.params.id === id) return recipe;
+    });
+    console.log(currentRecipe);
+
     return <p>I'm a single recipe</p>
   }
 }
 
-export default SingleRecipe;
+const mapStateToProps = state => {
+  return {
+    recipes: state.recipes.items
+  }
+}
+
+export default connect(mapStateToProps)(SingleRecipe);
