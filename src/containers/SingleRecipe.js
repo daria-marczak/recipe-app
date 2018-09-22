12fe83073b2b5880a2d3395efb95b09650ef9a35
daria marczak <marczak.ria@gmail.com>
2018-09-22 18:06:03 +0200

    Added mapping in render

--- src/containers/SingleRecipe.js
+++ src/containers/SingleRecipe.js
@@ -5,18 +5,15 @@ import "../styles/RecipeList.css";
 
 
 class SingleRecipe extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recipe: []
-    }
-  }
-
   componentDidMount() {
+    const { recipes } = this.props;
 
     if (!recipes) return <p>Loading</p>
+  }
 
+  render() {
     const { recipes } = this.props;
+
     const [currentRecipe] = recipes.filter(recipe => {
       const id = recipe.recipe.totalWeight.toString();
       if (this.props.match.params.id === id) return recipe;
@@ -24,29 +21,16 @@ class SingleRecipe extends Component {
 
     const recipe = currentRecipe.recipe;
 
-    this.setState({
-      recipe: currentRecipe.recipe
-    });
-
-
-  }
-
-  renderIngredients() {
-    // console.log(recipes);
-  }
-
-  render() {
-
-    console.log(this.state);
-
     return (
       <Row gutter={8}>
         <Col span={12}>
-          {/* <img src={recipe.image}></img> */}
+          <img src={recipe.image}></img>
         </Col>
         <Col span={12}>
-          {/* <h2>{recipe.label}</h2> */}
-          <Tag color="cyan"></Tag>
+          <h2>{recipe.label}</h2>
+          {recipe.healthLabels.map(label => {
+            return (<Tag color="cyan">{label}</Tag>)
+          })}
         </Col>
       </Row>
     )
