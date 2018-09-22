e413acf8986389afcc03ccded8827bca6bb360a2
daria marczak <marczak.ria@gmail.com>
2018-09-22 16:31:19 +0200

    Added routing

---
+++ src/containers/SingleRecipe.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SingleRecipe extends Component {
+  render() {
+    return <p>I'm a single recipe</p>
+  }
+}
+
+export default SingleRecipe;
--- src/index.js
+++ src/index.js
@@ -3,18 +3,31 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import thunk from "redux-thunk";
-import { createStore, applyMiddleware } from "redux";
-import reducers from "./reducers";
+import { createStore, compose, applyMiddleware } from "redux";
+import { createBrowserHistory } from "history";
+import { connectRouter, routerMiddleware, ConnectedRouter } from "connected-react-router";
+import { Route, Switch } from "react-router-dom";
 
+import reducers from "./reducers";
 import "./index.css";
 import App from "./App";
+import SingleRecipe from "./containers/SingleRecipe";
 
-const storeWithMiddleWare = applyMiddleware(thunk)(createStore);
+const history = createBrowserHistory();
+
+const store = createStore(
+  connectRouter(history)(reducers),
+  compose(applyMiddleware(routerMiddleware(history), thunk))
+);
 
 ReactDOM.render(
-  <Provider store={storeWithMiddleWare(reducers)}>
-    <App />
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path={`/:id`} component={SingleRecipe} />
+      </Switch>
+    </ConnectedRouter>
   </Provider>,
-
   document.getElementById("root")
 );
